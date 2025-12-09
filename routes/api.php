30f2627a1a4eacaa988b542bb098328d1b8abf0b
jabzs <kennethlapiz5@gmail.com>
2025-12-09 22:43:06 +0800

    feat: add API routes for authentication, products, carts, and orders

--- routes/api.php
+++ routes/api.php
@@ -13,7 +13,10 @@ Route::get('/user', function (Request $request) {
 
 // Public Product Routes
 Route::get('/products', [ProductController::class, 'index']);
+Route::post('/products', [ProductController::class, 'store']);
 Route::get('/products/{product}', [ProductController::class, 'show']);
+Route::put('/products/{product}', [ProductController::class, 'update']);
+Route::delete('/products/{product}', [ProductController::class, 'destroy']);
 
 // Cart Routes
 Route::prefix('cart')->group(function () {
